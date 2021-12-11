2a3f541ddfc28060ff18bd9841d15adcb93d7004
Espi <espi@riseup.net>
2021-12-11 00:07:34 -0500

    chore: remove github pages shit (it sucks)

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,18 @@
-module.exports = {
-  dir: {
-    input: "./src/",
-    output: "./dist/",
-  },
+module.exports = (eleventyConfig) => {
+  // Make Liquid capable of rendering "partials"
+  eleventyConfig.setLiquidOptions({
+    dynamicPartials: true,
+    strict_filters: true,
+  });
+
+  // Because we're running PostCSS as a separate process, Eleventy doesn't know when styles have changed
+  // Tell Eleventy to watch this CSS file so it can live-update changes into the browser for us
+  eleventyConfig.addWatchTarget("./dist/index.css");
+
+  return {
+    dir: {
+      input: "./src/",
+      output: "./dist/",
+    },
+  };
 };
