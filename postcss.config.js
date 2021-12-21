fc1c68f78ef7c7e61c4bea3d2fc9244ec686fa48
Espi <espi@riseup.net>
2021-12-21 09:42:07 -0500

    feat: add my new-ish eleventy-based site utilising sakura.css

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,31 @@
 const purgecss = require("@fullhuman/postcss-purgecss");
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
-const tailwindcss = require("tailwindcss");
+const postcssImport = require("postcss-import");
 
 const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
+// Configures purgeCSS
+const purgeCSSConfig = {
+  content: ["./src/**/**/*.{liquid,js}"],
+  css: ["./_site/css/**/**/*.css"],
+  fontFace: true,
+  variables: true,
+  keyframes: true,
+};
+
 module.exports = {
   plugins: [
-    tailwindcss,
-    IS_PRODUCTION ? autoprefixer : undefined,
-    IS_PRODUCTION ? cssnano({ preset: "default" }) : undefined,
+    // Enables imports from node_modules/
+    postcssImport({ from: "./src/assets/css" }),
 
-    purgecss({
-      content: ["./src/**/*.liquid", "./src/**/*.css"],
-      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-    }),
+    // Vendor-specific prefixes
+    autoprefixer(),
+
+    // Purges unused CSS
+    IS_PRODUCTION ? purgecss(purgeCSSConfig) : undefined,
+
+    // Minifies CSS
+    IS_PRODUCTION ? cssnano() : undefined,
   ],
 };
