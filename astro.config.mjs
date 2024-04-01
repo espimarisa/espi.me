c927f76b5291493a30da99946381665c676bea10
Espi Marisa <contact@espi.me>
2024-04-01 00:18:32 -0500

    fix vercel :c

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import compress from "astro-compress";
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
+  output: "server",
   site: "https://espi.me",
   integrations: [compress()],
   adapter: vercel(),
