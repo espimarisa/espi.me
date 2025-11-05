c6bbb8586c0c8613b28db63bad3ea7a46c80ab89
Espi Marisa <hi@espi.me>
2025-11-05 16:27:36 -0600

    fix: rm old stuff
    bleh

--- src/utils/accounts.ts
+++ src/utils/accounts.ts
@@ -19,24 +19,6 @@ export const accounts = {
     platform: "GitHub",
     username: "espimarisa",
   },
-  x: {
-    href: "https://x.com/espimarisa_",
-    icon: "simple-icons:x",
-    platform: "X",
-    username: "espimarisa_",
-  },
-  bluesky: {
-    href: "https://bsky.app/profile/did:plc:tuihqhadazqooxe5fhw6dhzk",
-    icon: "simple-icons:bluesky",
-    platform: "Bluesky",
-    username: "espi.me",
-  },
-  discord: {
-    href: "https://discord.com/users/647269760782041133",
-    icon: "simple-icons:discord",
-    platform: "Discord",
-    username: "espimarisa",
-  },
   telegram: {
     href: "https://t.me/espimarisa",
     icon: "simple-icons:telegram",
